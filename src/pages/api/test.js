db23a94bee6305ac14bb4fb67a3ef906c37a39ed
aleksey <overby999@gmail.com>
2021-10-30 16:05:36 +0300

    30.10.21 16:05

---
+++ src/pages/api/test.js
@@ -0,0 +1,5 @@
+function test(req, res) {
+  res.status(200).json({name: 'robbin 12323'});
+}
+
+export default test;
--- src/pages/sasha.js
+++ src/pages/sasha.js
@@ -1,1 +1,16 @@
-const url = 'https://jsonplaceholder.typicode.com/posts/1';
+import {useEffect, useState} from 'react';
+
+function Sasha() {
+  const [name, setName] = useState('anon');
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('/api/test');
+      const json = await response.json();
+      setName(json.name);
+    })();
+  }, []);
+  return <div>name is {name}</div>;
+}
+
+export default Sasha;
