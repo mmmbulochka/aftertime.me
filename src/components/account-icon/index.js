0169fa11ea27548e06ac07e5c351492d4181b2ac
Malyuta Aleksey <overby999@gmail.com>
2022-01-01 23:19:19 +0300

    01.01.22 23:19

--- src/components/account-icon/index.js
+++ src/components/account-icon/index.js
@@ -1,14 +1,7 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import style from './/index.module.css';
 import AccountModal from '../account-modal';
-
-AccountModal.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
-  selectedValue: PropTypes.string.isRequired,
-};
 
 function AccountIcon() {
   const [open, setOpen] = React.useState(false);
