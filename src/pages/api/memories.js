a5c8646ebe6564687093aa95168ade2d894c23c8
aleksey <overby999@gmail.com>
2022-01-11 19:00:23 +0300

    11.01.22 19:00

--- src/pages/api/memories.js
+++ src/pages/api/memories.js
@@ -4,7 +4,29 @@ async function memories(req, res) {
   await mongo.client.connect();
   const memories = await mongo.db.collection('memories').find({}).toArray();
 
-  res.send(memories);
+  const preparedMemories = await Promise.all(
+    memories.map(async (memory) => {
+      return {
+        id: memory._id,
+        files: await Promise.all(
+          memory.files.map(async (file) => {
+            const fileDb = (
+              await mongo.gridFs
+                .find({
+                  _id: file,
+                })
+                .toArray()
+            )[0];
+            return `http://${req.headers.host}/api/files/${file.toString()}/${
+              fileDb.filename
+            }`;
+          })
+        ),
+      };
+    })
+  );
+
+  res.send(preparedMemories);
 }
 
 export default memories;
