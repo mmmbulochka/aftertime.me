91c285bd8a570513e2f71056c4a68b45aca2d9ad
utftufutukgyftryidytftuv <overby999@gmail.com>
2022-02-09 00:03:22 +0300

    09.02.22 00:03

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,39 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {styletron} from '../styletron';
 
 class MyDocument extends Document {
+  static async getInitialProps(context) {
+    const renderPage = () =>
+      context.renderPage({
+        enhanceApp: (App) => (props) =>
+          (
+            <StyletronProvider value={styletron}>
+              <App {...props} />
+            </StyletronProvider>
+          ),
+      });
+
+    const initialProps = await Document.getInitialProps({
+      ...context,
+      renderPage,
+    });
+    const stylesheets = styletron.getStylesheets() || [];
+    return {...initialProps, stylesheets};
+  }
   render() {
     return (
       <Html>
         <Head>
+          {this.props.stylesheets.map((sheet, i) => (
+            <style
+              className='_styletron_hydrate_'
+              dangerouslySetInnerHTML={{__html: sheet.css}}
+              media={sheet.attrs.media}
+              data-hydrate={sheet.attrs['data-hydrate']}
+              key={i}
+            />
+          ))}
           <link
             href='https://fonts.googleapis.com/css2?family=Lato&display=swap'
             rel='stylesheet'
