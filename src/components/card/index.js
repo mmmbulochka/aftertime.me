61a26cf32d058269f5a7e901f79d8e5090defb02
utftufutukgyftryidytftuv <overby999@gmail.com>
2022-02-08 21:12:13 +0300

    fix cards

---
+++ src/components/card/index.js
@@ -0,0 +1,53 @@
+import CardMedia from '@mui/material/CardMedia';
+import * as React from 'react';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import dayjs from 'dayjs';
+import s from './index.module.css';
+
+function Card(props) {
+  return (
+    <div className={s.card}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: 200,
+          height: 200,
+        }}
+      >
+        <CardContent sx={{flex: '1 0 auto'}}>
+          <Typography component='div' variant='h5'>
+            {props.memory.title}
+          </Typography>
+          <Typography
+            variant='subtitle1'
+            color='text.secondary'
+            component='div'
+          >
+            {dayjs(props.memory.created * 1000).format('DD/MM/YYYY')} -{' '}
+            {dayjs(props.memory.date * 1000).format('DD/MM/YYYY')}
+          </Typography>
+        </CardContent>
+        <div
+          style={{
+            borderRadius: '100%',
+            backgroundColor: props.isReady ? '#DCEDC8' : '#FFCDD2',
+            height: 20,
+            width: 20,
+            marginLeft: 10,
+            marginBottom: 10,
+          }}
+        />
+      </div>
+      <CardMedia
+        component='img'
+        sx={{width: 200, height: 200, marginLeft: 'auto'}}
+        image={props.memory.icon}
+        alt='Live from space album cover'
+      />
+    </div>
+  );
+}
+
+export default Card;
--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
+// import Card from '@mui/material/Card';
+import Card from 'src/components/card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import dayjs from 'dayjs';
 import ContainerModal from '../container-modal';
+import style from './index.module.css';
 
 export default function MediaControlCard(props) {
   const [open, setOpen] = React.useState(false);
@@ -17,51 +19,58 @@ export default function MediaControlCard(props) {
   };
   const handleClose = () => setOpen(false);
   return (
-    <div>
-      <Card
-        onClick={handleOpen}
-        sx={{
-          display: 'flex',
-          width: 370,
-          height: 160,
-          boxShadow: '2px 5px 19px 2px rgba(219, 224, 229, 0.2)',
-          borderRadius: 0,
-        }}
-      >
-        <Box sx={{display: 'flex', flexDirection: 'column'}}>
-          <CardContent sx={{flex: '1 0 auto'}}>
-            <Typography component='div' variant='h5'>
-              {props.memory.title}
-            </Typography>
-            <Typography
-              variant='subtitle1'
-              color='text.secondary'
-              component='div'
-            >
-              {dayjs(props.memory.created * 1000).format('DD/MM/YYYY')} -{' '}
-              {dayjs(props.memory.date * 1000).format('DD/MM/YYYY')}
-            </Typography>
-          </CardContent>
-          <div
-            style={{
-              borderRadius: '100%',
-              backgroundColor: isReady ? '#DCEDC8' : '#FFCDD2',
-              height: 20,
-              width: 20,
-              marginLeft: 10,
-              marginBottom: 10,
-            }}
-          />
-        </Box>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Card memory={props.memory} isReady={isReady} />
+      {/*<Card*/}
+      {/*  className={style.card}*/}
+      {/*  onClick={handleOpen}*/}
+      {/*  sx={{*/}
+      {/*    display: 'flex',*/}
+      {/*    maxWidth: 370,*/}
+      {/*    // height: 160,*/}
+      {/*    boxShadow: '2px 5px 19px 2px rgba(219, 224, 229, 0.2)',*/}
+      {/*    borderRadius: 0,*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  <Box sx={{display: 'flex', flexDirection: 'column'}}>*/}
+      {/*    <CardContent sx={{flex: '1 0 auto'}}>*/}
+      {/*      <Typography component='div' variant='h5'>*/}
+      {/*        {props.memory.title}*/}
+      {/*      </Typography>*/}
+      {/*      <Typography*/}
+      {/*        variant='subtitle1'*/}
+      {/*        color='text.secondary'*/}
+      {/*        component='div'*/}
+      {/*      >*/}
+      {/*        {dayjs(props.memory.created * 1000).format('DD/MM/YYYY')} -{' '}*/}
+      {/*        {dayjs(props.memory.date * 1000).format('DD/MM/YYYY')}*/}
+      {/*      </Typography>*/}
+      {/*    </CardContent>*/}
+      {/*    <div*/}
+      {/*      style={{*/}
+      {/*        borderRadius: '100%',*/}
+      {/*        backgroundColor: isReady ? '#DCEDC8' : '#FFCDD2',*/}
+      {/*        height: 20,*/}
+      {/*        width: 20,*/}
+      {/*        marginLeft: 10,*/}
+      {/*        marginBottom: 10,*/}
+      {/*      }}*/}
+      {/*    />*/}
+      {/*  </Box>*/}
 
-        <CardMedia
-          component='img'
-          sx={{width: 151, marginLeft: 'auto'}}
-          image={props.memory.icon}
-          alt='Live from space album cover'
-        />
-      </Card>
-      <ContainerModal memory={props.memory} open={open} onClose={handleClose}/>
+      {/*  <CardMedia*/}
+      {/*    component='img'*/}
+      {/*    sx={{width: 151, marginLeft: 'auto'}}*/}
+      {/*    image={props.memory.icon}*/}
+      {/*    alt='Live from space album cover'*/}
+      {/*  />*/}
+      {/*</Card>*/}
+      <ContainerModal memory={props.memory} open={open} onClose={handleClose} />
     </div>
   );
 }
